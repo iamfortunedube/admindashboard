d8c7f056cff0cca6db7517dcfef8eaeff25fc957
spha1234 <sphafortunedube@outlook.com>
2018-02-26 21:05:29 +0200

    Donations

---
+++ inc/blockedUsersContent.php
@@ -0,0 +1,4 @@
+<div class="welcomeTitle">
+    <h3 style='font-family: "Comic Sans MS", cursive, sans-serif; text-align: center'>Blocked Users</h3>
+</div>
+
---
+++ inc/dashContent.php
@@ -0,0 +1,43 @@
+<div class="welcomeTitle">
+    <h3 style='font-family: "Comic Sans MS", cursive, sans-serif; text-align: center'>Dashboard</h3>
+</div>
+<div class="wrapper">
+    <div class="row">
+        <div class="col-md-3 stretch-card grid-margin">
+            <div class="card bg-gradient-success text-white">
+                <div class="card-body">
+                    <h4 class="font-weight-normal mb-3">Today's Transactions</h4>
+                    <h2 class="font-weight-normal mb-5">152</h2>
+                    <p class="card-text">Increased by 30%</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 stretch-card grid-margin">
+            <div class="card bg-gradient-warning text-white">
+                <div class="card-body">
+                    <h4 class="font-weight-normal mb-3">Active Users</h4>
+                    <h2 class="font-weight-normal mb-5">3 542</h2>
+                    <p class="card-text">Incresed by 40%</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 stretch-card grid-margin">
+            <div class="card bg-gradient-info text-white">
+                <div class="card-body">
+                    <h4 class="font-weight-normal mb-3">Blocked Users</h4>
+                    <h2 class="font-weight-normal mb-5">125</h2>
+                    <p class="card-text">Decreased by 10%</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 stretch-card grid-margin">
+            <div class="card bg-gradient-success text-white">
+                <div class="card-body">
+                    <h4 class="font-weight-normal mb-3">Completed Transactions</h4>
+                    <h2 class="font-weight-normal mb-5">8 365</h2>
+                    <p class="card-text">Increased by 5%</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

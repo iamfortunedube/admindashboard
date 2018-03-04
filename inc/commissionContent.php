5beda6e04603d1af89700986fe436285d734390b
spha1234 <sphafortunedube@outlook.com>
2018-03-04 17:41:28 +0200

    Change

--- inc/commissionContent.php
+++ inc/commissionContent.php
@@ -1,3 +1,3 @@
 <div class="welcomeTitle mainTitle">
-    <h3 style='font-family: "Comic Sans MS", cursive, sans-serif; text-align: center'>Commission</h3>
+    <h3 style='font-family: "Comic Sans MS", cursive, sans-serif; text-align: center'>Commissions</h3>
 </div>

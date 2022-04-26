e68cebf2ca752510e9004560aa02ed99d0e30a04
Melusi Maphumulo <melusimaphumulo@gmail.com>
2022-04-26 10:36:29 +0200

    fixed donation transaction status updates

--- donations.php
+++ donations.php
@@ -59,7 +59,7 @@
                     if($remaing_don_amount > $remaining_claim_amount){
                         $remaining_don = $remaing_don_amount - $remaining_claim_amount;
                         
-                        $update_donation = "Update donation SET remaining_don = '".$remaining_don."'  WHERE id = ".@$donator_id.";";
+                        $update_donation = "Update donation SET remaining_don = '".$remaining_don."',status '1'  WHERE id = ".@$donator_id.";";
                         $ress = mysqli_query($conn,$update_donation);
 
                         $update_claims = "Update claims SET remaining_claim = 0,states = 2 WHERE id = ".@$claimer.";";
@@ -323,25 +323,35 @@
                         <td><input type="hidden" name="don_id" value="'.$row['id'].'" hidden/>'.$row['id'].'</td>
                         <td><input type="hidden" name="cell_donator" value="'.$row['cellDonator'].'" hidden/>'.$row['cellDonator'].'</td>
                         <td><input type="hidden" name="amount" value="'.$row['amount'].'" hidden/>'.$row['amount'].'</td>
-                        <td><input type="hidden" name="don_date" value="'.$row['donDate'].'" hidden/>'.$row['donDate'].'</td>
+                        <td><input type="hidden" name="don_date" value="'.$row['donDate'].'" hidden/>'.$row['donDate'].'</td>';
                         
-                        <td> <select name="claim_option" class="form-control" style="border-color: rgb(218,165,32); border-style: dashed; border-width: 2px;">';
+                       
                         $stmnt = "Select c.id AS 'claim_id',fname,remaining_claim from claims C,users S where C.cellClaim = S.p_number AND remaining_claim > 0";
                         $results = mysqli_query($conn,$stmnt);
                         if(mysqli_num_rows($results) > 0){
+                            echo '<td> <select name="claim_option" class="form-control" style="border-color: rgb(218,165,32); border-style: dashed; border-width: 2px;">';
                             while($rows = mysqli_fetch_assoc($results)){
                                 
                                 $fnameC = $rows['claim_id'];
                             echo '
                                 <option value="'.@$fnameC.'">'.$rows['fname'].' R'.$rows['remaining_claim'].'</option>';
+                                
                             }
-                        }echo '
-                        </select></td>';
-                        if(!$row['remaining_don']=="0" && mysqli_num_rows($results) <= 0){
-                         echo '<td><input type="submit" name="submit" class="btn btn-success" value="Allocate"/></td>';
+
+                            echo '
+                            </select></td>';
+                        }else{
+                            echo '
+                            <td> No Claims</td>';
                         }
-                        else{
-                        echo  '<td>Allocated</td>';
+
+                        if(!(mysqli_num_rows($results) > 0)){
+                            echo  '<td>No Action</td>';
+                        }else if(!$row['remaining_don'] == "0"){
+                            echo '<td><input type="submit" name="submit" class="btn btn-success" value="Allocate"/></td>';
+                        }
+                       else{
+                            echo  '<td>Allocated</td>';
                         }
                    echo '
                    <td><input type="text" name="remaining_don" value="'.$row['remaining_don'].'" hidden/>'.$row['remaining_don'].'</td>
